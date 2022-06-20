ebe3a24ade50b75563a5491c5dd070a5e38d809d
sainingo <nickmentor421@gmail.com>
2022-06-20 17:23:15 +0300

    Add person,student,teacher classes

---
+++ teacher.rb
@@ -0,0 +1,11 @@
+require './person'
+class Teacher < Person
+  def initialize(age, name = 'unknown', parent_permision: true)
+    super(age, name, parent_permision)
+    @specialization = specialization
+  end
+
+  def can_use_services
+    true
+  end
+end
