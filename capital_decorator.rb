8c59c5e644238e9f07eab501e9b151aad5fcb93f
sainingo <nickmentor421@gmail.com>
2022-06-22 12:34:53 +0300

    Remove constructor method

--- capital_decorator.rb
+++ capital_decorator.rb
@@ -1,11 +1,6 @@
 require './base_decorator'
 
 class CapitalDecorator < Decorator
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
-
   def correct_name
     @nameable.correct_name.capitalize
   end
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,10 +1,6 @@
 require './base_decorator'
 
 class TrimmerDecorator < Decorator
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
 
   def correct_name
     @nameable.correct_name.slice(0, 10)
