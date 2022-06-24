9fdb8d32d62b08dc4585257a944d9c26429c3817
sainingo <nickmentor421@gmail.com>
2022-06-24 10:59:21 +0300

    Create functionalities

--- app.rb
+++ app.rb
@@ -70,4 +70,88 @@ class App
     gets
     run
   end
+
+  def my_permission(my_char)
+    case my_char
+    when 'N'
+      false
+    when 'Y'
+      true
+    end
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    teacher = Teacher.new(age, name, nil, specialization)
+    @people.push(teacher)
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    permission = gets.chomp
+    student = Student.new(age, name, my_permission(permission), nil)
+    @people.push(student)
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    person_type = gets.chomp
+    case person_type
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    end
+    puts "Person created successfully\n"
+    gets
+    run
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    new_book = Book.new(title, author)
+    @my_books.push(new_book)
+    run
+  end
+
+  def create_rental
+    puts "\nSelect a book from the following list by number"
+    list_books
+    book_index = gets.chomp
+    puts "\nSelect a person from the following list by number"
+    list_people
+    person_index = gets.chomp
+    print "\n Date(yyyy/mm/dd): "
+    rental_date = gets.chomp
+    new_rental = Rental.new(rental_date, @my_books[book_index.to_i], @people[person_index.to_i])
+    @my_rentals.push(new_rental)
+    puts 'Rental added successfully'
+    run
+  end
+
+  def list_rental
+    me = nil
+    print "\nID of person: "
+    person_id = gets.chomp
+    @people.each do |x|
+      me = x if x.id == person_id.to_i
+    end
+    me.rentals.each do |x|
+      puts x.date
+    end
+    puts
+    run
+  end
 end
