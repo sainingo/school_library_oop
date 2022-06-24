795def66323e48025deab4889967c5c02299cef9
sainingo <nickmentor421@gmail.com>
2022-06-24 10:18:17 +0300

    Create a class app

---
+++ app.rb
@@ -0,0 +1,13 @@
+require './person'
+require './book'
+require './student'
+require './teacher'
+require './rental'
+
+class App
+  def initialize
+    @my_books = []
+    @my_rentals = []
+    @people = []
+  end
+end
