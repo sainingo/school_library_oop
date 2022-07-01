cd98e1a97fbf2ce6ae6de9f1791a7ec9ad7996fe
sainingo <nickmentor421@gmail.com>
2022-07-01 10:41:21 +0300

    adding a new rentals test case

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -2,10 +2,13 @@ require_relative '../rental'
 
 describe Rental do
   context 'When providing a book title and a person id' do
-    rented = Rental.new('01-02-2022', 'BookTitle', 2)
-
-    it 'Should create rental for person' do
-      expect(rented).to be_an_instance_of Rental
+    it 'should be able to create a rental' do
+      person = Person.new(20, 'John')
+      book = Book.new('Harry Potter', 'JK Rowling')
+      rental = Rental.new('2015/1/1', person, book)
+      expect(rental.date).to eq('2015/1/1')
+      expect(rental.person).to eq(person)
+      expect(rental.book).to eq(book)
     end
   end
 end
